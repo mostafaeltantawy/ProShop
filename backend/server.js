a72bdc503eb6e5d09f59e95f4b3a3c8e97c269b9
mostafaeltantawy <eltantawymostafa2@gmail.com>
2024-01-01 16:52:31 +0200

    Made a custom Error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 import connectDb from './config/db.js';
 const port = process.env.PORT || 5000;
@@ -9,5 +10,8 @@ connectDb(); //connect to mongodb
 const app = express();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is run on port ${port}`));
