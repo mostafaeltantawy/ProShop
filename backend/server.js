b76f2246de062e566fad8376bfb22644d4a435c2
mostafaeltantawy <eltantawymostafa2@gmail.com>
2024-01-01 16:34:07 +0200

    Add products route and async handler

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,13 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 import connectDb from './config/db.js';
 const port = process.env.PORT || 5000;
 
 connectDb(); //connect to mongodb
 const app = express();
-app.get('/', (req, res) => {
-  res.send('API is Running');
-});
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/product/:id', (req, res) => {
-  const productId = req.params.id;
-  const product = products.find((p) => p._id === req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.send('Couldnon find this product');
-  }
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server is run on port ${port}`));
